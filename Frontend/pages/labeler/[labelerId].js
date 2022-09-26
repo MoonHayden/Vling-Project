95258546da71dd3f8ba2ece7dd7e75aba5e45072
junki <junki646@gmail.com>
2022-09-26 18:17:15 +0900

    Add : 라벨러 전체목록 구현

--- Frontend/pages/labeler/[labelerId].js
+++ Frontend/pages/labeler/[labelerId].js
@@ -2,7 +2,31 @@ import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 function labelerDetail() {
-  return;
+  return ();
+
+
+  const router = useRouter();
+  const labelerId = router.query.labelerId;
+
+  return (
+    <Wrap>
+      {labelerId}
+      <div>
+        진행중인 테스크
+        <div>
+          <div>Task Name3</div>
+          <div>Task Name2</div>
+          <div>Task Name54</div>
+          <div>Task Name71</div>
+        </div>
+      </div>
+    </Wrap>
+  );
 }
 
-export default labelerDetail;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+  export default labelerDetail;
