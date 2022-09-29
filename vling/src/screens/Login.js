b55a020322b2a4909fb14b69a69ea930b0629fb5
MoonHayden <hdmoon127@gmail.com>
2022-09-29 22:43:19 +0900

    revise

---
+++ vling/src/screens/Login.js
@@ -0,0 +1,116 @@
+import React, {useState, useEffect} from 'react';
+import {View, Image, Button, StyleSheet, Text} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+export default function Login({navigation}) {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '937607842726-j4s6oq4anspnfi1gr99mj1uckpub529j.apps.googleusercontent.com',
+      offlineAccess: true,
+      forceCodeForRefreshToken: true,
+    });
+    isSignedIn();
+  }, []);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('due___', userInfo);
+      setUser(userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        console.log('error1', error);
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+        console.log('error2', error);
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+        console.log('error3', error);
+      } else {
+        // some other error happened
+        console.log('error', error);
+      }
+    }
+  };
+
+  const isSignedIn = async () => {
+    const isSignedIn = await GoogleSignin.isSignedIn();
+    if (!!isSignedIn) {
+      getCurrentUserInfo();
+    } else {
+      console.log('please login');
+    }
+  };
+
+  const getCurrentUserInfo = async () => {
+    try {
+      const userInfo = await GoogleSignin.signInSilently();
+      console.log('edit___', user);
+      setUser(userInfo);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+        alert('User has not signed in yet');
+        console.log('User has not signed in yet');
+      } else {
+        alert('Someting went wrong.');
+        console.log('Someting went wrong.');
+      }
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setUser({});
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View style={styles.login}>
+      <Image
+        style={styles.logo}
+        source={require('/Users/haydenmoon/Desktop/vling/src/asset/images/logo.png')}
+      />
+      <View>
+        {!user.idToken ? (
+          <GoogleSigninButton style={styles.Google} onPress={signIn} />
+        ) : (
+          <TouchableOpacity onPress={signOut}>
+            <Text>Signout</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+      <Button
+        title="Google Social Login"
+        onPress={() => navigation.reset({routes: [{name: 'MainScreen'}]})}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Google: {width: 250, height: 50},
+  logo: {
+    fontSize: 20,
+  },
+  login: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 100,
+    backgroundColor: '#FF0044',
+  },
+});
---
+++ vling/src/screens/Mypage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
+
+export default function Mypage() {
+  return (
+    <View style={styles.mypage}>
+      <Text style={styles.text}>Logout</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mypage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'blue',
+  },
+});
