0612b69cd52800b2f14f5b6ebd143f923495e212
junki <junki646@gmail.com>
2022-10-04 11:23:11 +0900

    Modify : 주석 삭제

--- Frontend/components/apollo-client.js
+++ Frontend/components/apollo-client.js
@@ -1,5 +1,4 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { HttpLink } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://www2.wecode.buzzntrend.com:4000/graphql',
   ssrMode: true,
