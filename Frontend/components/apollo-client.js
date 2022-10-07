f1f77b2c61722ac142119c68f2a98ff110066b0c
WH <wonho@WHui-MacBookPro.local>
2022-10-07 17:58:25 +0900

    수정

--- Frontend/components/apollo-client.js
+++ Frontend/components/apollo-client.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://www2.wecode.buzzntrend.com:4000/graphql',
-  ssrMode: true,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
 });
 
 export default client;
