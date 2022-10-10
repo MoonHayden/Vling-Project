e66bdc1a19f4d815ff0cee70f9daeb5b08c21aaf
junki <junki646@gmail.com>
2022-10-11 03:14:47 +0900

    Modify : 메뉴탭 sort 동적으로 변경

--- Frontend/pages/labeler/_components/TitleTab.js
+++ Frontend/pages/labeler/_components/TitleTab.js
@@ -1,58 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
-import sortDefaultImg from '../../../public/images/sortDefault.png';
-import sortUpImg from '../../../public/images/sortUp.png';
-import sortDownImg from '../../../public/images/sortDown.png';
+import sortDefault from '../../../public/images/sortDefault.png';
+import sortUp from '../../../public/images/sortUp.png';
+import sortDown from '../../../public/images/sortDown.png';
 import { useState } from 'react';
 const TitleTab = ({ labelers, setLabelers }) => {
   const TITLE_LIST = ['email', 'value'];
 
   const [sortType, setSortType] = useState({
-    email: sortDefaultImg,
-    value: sortDefaultImg,
+    email: sortDefault,
+    value: sortDefault,
   });
 
   const sortGogo = title => {
-    if (sortType[title] === sortDefaultImg) {
-      sortUp(title);
-    } else if (sortType[title] === sortUpImg) {
-      sortDown(title);
-    } else if (sortType[title] === sortDownImg) {
-      sortDefault(title);
+    if (sortType[title] === sortDefault) {
+      setLabelers(upSortedLabelers(title));
+      changeSortType(title, sortUp);
+    } else if (sortType[title] === sortUp) {
+      setLabelers(downSortedLabelers(title));
+      changeSortType(title, sortDown);
+    } else if (sortType[title] === sortDown) {
+      setLabelers(defaultLabelers());
+      changeSortType(title, sortDefault);
     }
   };
 
-  const sortUp = type => {
-    const sortedLabelers = [...labelers].sort((a, b) =>
-      a[type].toLowerCase() < b[type].toLowerCase() ? -1 : 1
+  const upSortedLabelers = title => {
+    return [...labelers].sort((a, b) =>
+      a[title].toLowerCase() < b[title].toLowerCase() ? -1 : 1
     );
-    setLabelers(sortedLabelers);
-    setSortType({ ...sortType, [type]: sortUpImg });
   };
 
-  const sortDown = type => {
-    const sortedLabelers = [...labelers].sort((a, b) =>
-      a[type].toLowerCase() < b.email.toLowerCase() ? 1 : -1
+  const downSortedLabelers = title => {
+    return [...labelers].sort((a, b) =>
+      a[title].toLowerCase() < b.email.toLowerCase() ? 1 : -1
     );
-    setLabelers(sortedLabelers);
-    setSortType({ ...sortType, [type]: sortDownImg });
   };
 
-  const sortDefault = type => {
-    const sortedLabelers = [...labelers].sort((a, b) =>
-      a._id.toLowerCase() < b._id.toLowerCase() ? -1 : 1
-    );
-    setLabelers(sortedLabelers);
-    setSortType({ ...sortType, [type]: sortDefaultImg });
+  const defaultLabelers = () => {
+    return [...labelers].sort((a, b) => (a._id < b._id ? -1 : 1));
+  };
+
+  const changeSortType = (title, src) => {
+    const tmp_sortType = { ...sortType };
+
+    for (var key in sortType) {
+      key === title
+        ? (tmp_sortType[key] = src)
+        : (tmp_sortType[key] = sortDefault);
+    }
+    setSortType(tmp_sortType);
   };
 
   return (
     <Wrap>
-      {TITLE_LIST.map(title => (
-        <TitleBox onClick={() => sortGogo(title)}>
+      {TITLE_LIST.map((title, idx) => (
+        <TitleBox onClick={() => sortGogo(title)} key={idx}>
           <ImageWrap>
-            <Image src={sortType[title]} width={20} height={20} alt="sort" />
+            <Image src={sortType[title]} width={22} height={20} alt="sort" />
           </ImageWrap>
           {title}
         </TitleBox>
@@ -65,9 +71,9 @@ export default TitleTab;
 
 const Wrap = styled.div`
   display: flex;
+
   width: 100%;
   height: 3rem;
-  align-items: center;
   justify-content: space-between;
   padding: 0 3rem 0 1rem;
   margin-top: 0.5rem;
